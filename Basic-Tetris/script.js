e6da399283500d05f438b74d0b4db91c10fd77be
johnsonkim2000 <Johnsonkim2000@gmail.com>
2022-09-30 11:07:01 -0400

    Shapes Tetris Update

---
+++ Basic-Tetris/script.js
@@ -0,0 +1,8 @@
+const lTetromino = 'firstShape'
+const zTetromino = 'secondShape'
+const oTetromino = 'thirdShape'
+const iTetromino = 'fourthShape'
+const tTetromino = 'fifthShape'
+
+const tetrominoes = [lTetromino, zTetromino, oTetromino, iTetromino, tTetromino]
+console.log(tetrominoes[5])
